436d744b49b25e60566b77bd3bc1e63c4c6cbd34
mrmomenticus <vova.bod53@gmail.com>
2022-03-18 15:38:41 +0300

    #2 Создан класс для чтения и передачи данных

--- main.cpp
+++ main.cpp
@@ -1,45 +1,64 @@
 #include <iostream>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <fstream>
 #include <thread>
 #include <mutex>
-#include <vector>
-//Необходимо реализовать клиент-серверное приложение под Linux. 
-//Клиент - программа, запускаемая из консоли. 
-//Сервер - демон, корректно завершающийся по сигналам SIGTERM и SIGHUP. 
-//Клиент должен передать содержимое текстового файла через TCP. 
-//Сервер должен принять и сохранить в файл.
-void fileReader(char *buffer) {
-    std::ifstream file;
-    file.open("\\home\\mrmomenticus\\CLionProjects\\client\\file");
-    while (true) {
-        file.getline(buffer, 1024);
-        if (!file.eof()) {
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <bits/socket.h>
+#include <fstream>
+
+class ReadAndTake{
+    char m_buffer[1024];
+    std::mutex m;
+public:
+    void reader() { //разбиваем считанный файл на пакеты
+        std::ifstream file;
+        file.open("/home/mrmomenticus/CLionProjects/Client/test");
+        if (!file.is_open()) // если файл не открыт
+            std::cout << "Файл не может быть открыт!\n"; // сообщить об этом
+        else
+        {
+            file.getline(m_buffer, 1024);
+            std::cout << m_buffer;
             file.close();
-            break;
         }
     }
-}
+        void take(){
+            int sock= socket(AF_INET, SOCK_STREAM, 0);
+            if(sock < 0)
+            {
+                std::cout << "Не получилось создать сокет\n";
+                exit(1);
+            }
+            struct sockaddr_in addr;
+            addr.sin_family = AF_INET;
+            addr.sin_port = htons(3425);
+            addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+            if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+            {
+                std::cout << "Не получилось связать\n";
+                exit(2);
+            }
+            m.lock();
+            int send_result = send(sock, m_buffer, 1024, 0);
+            if(send_result <= 0)
+                std::cout << "Не получилось передать данные" << std::endl;
+            m.unlock();
+
+            close(sock);
+    }
+};
 int main() {
-    std::mutex m;
     setlocale(LC_ALL, "RUS");
-    char buffer[1024];
-    std::thread t1([&buffer]() { fileReader(buffer); });
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << buffer[3];
-//    int sock =  socket(AF_INET, SOCK_STREAM,0); //Создаем сокет, используем SOCK_STREAM для TCP
-//    struct sockaddr_in adr = {0};
-//
-//    adr.sin_family = AF_INET;
-//    adr.sin_port = htons(3425);
-//    if(connect(sock, (struct sockaddr *)&adr, sizeof(adr)) < 0) //Проверка на коннект
-//        {
-//        std::cout << "Не получилось связать\n";
-//        exit(2);
-//        }
-//    listen(sock, 1);
+    ReadAndTake objecktReadandTake;
+    std::thread t1([&]() { objecktReadandTake.reader(); });
+
+   // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+
 
 
 
