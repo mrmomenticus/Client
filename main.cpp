fd4842b66e2c0c3bce7ef13f6d5b804d7306ef53
mrmomenticus <vova.bod53@gmail.com>
2022-03-23 19:20:53 +0300

    #3 Исправлена отправка данных

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
 #include <string>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,56 +8,43 @@
 #include <bits/socket.h>
 #include <fstream>
 
-class ReadAndTake{
-    char m_buffer[1024];
-    std::mutex m;
-public:
-    void reader() { //разбиваем считанный файл на пакеты
-        std::ifstream file;
-        file.open("/home/mrmomenticus/CLionProjects/Client/test");
-        if (!file.is_open()) // если файл не открыт
-            std::cout << "Файл не может быть открыт!\n"; // сообщить об этом
-        else
-        {
-            file.getline(m_buffer, 1024);
-            std::cout << m_buffer;
-            file.close();
-        }
-    }
-        void take(){
-            int sock= socket(AF_INET, SOCK_STREAM, 0);
-            if(sock < 0)
-            {
-                std::cout << "Не получилось создать сокет\n";
-                exit(1);
-            }
-            struct sockaddr_in addr;
-            addr.sin_family = AF_INET;
-            addr.sin_port = htons(3425);
-            addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-            if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-            {
-                std::cout << "Не получилось связать\n";
-                exit(2);
-            }
-            m.lock();
-            int send_result = send(sock, m_buffer, 1024, 0);
-            if(send_result <= 0)
-                std::cout << "Не получилось передать данные" << std::endl;
-            m.unlock();
-
-            close(sock);
-    }
-};
 int main() {
+
+    std::ifstream file;
+    char buffer[256];
+    int flag = 0;
     setlocale(LC_ALL, "RUS");
-    ReadAndTake objecktReadandTake;
-    std::thread t1([&]() { objecktReadandTake.reader(); });
+    int sock= socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        std::cout << "Не получилось создать сокет\n";
+        exit(1);
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        std::cout << "Не получилось связать\n";
+        exit(2);
+    }
+    file.open("/home/mrmomenticus/CLionProjects/Client/test1");
+    if (!file.is_open()) { // если файл не открыт
+        std::cout << "Файл не может быть открыт!\n";
+        exit(0);
+    }
+    while(!file.eof()) {
+        file.read(  buffer, 256);
+        int send_result = send(sock, buffer, 256, 0);
+        if (send_result <= 0) {
+            std::cout << "Не получилось передать данные" << std::endl;
+            close(sock);
+        }
 
-   // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-
-
-
-
+    }
+    std::cout <<"Отправлено";
+    file.close();
+    close(sock);
+    exit(0);
 }
